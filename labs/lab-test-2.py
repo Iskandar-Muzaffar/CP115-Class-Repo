a20920f606c85c9ce88b924da2fcda7e572f99d4
Iskandar123123 <iskandarmuzaffar999@gmail.com>
2025-08-19 13:09:56 +0800

    update

---
+++ labs/lab-test-2.py
@@ -0,0 +1,11 @@
+# Iskandar's Lab Test 2
+# This code demonstrates various string operations and formatting in Python.
+
+Data = "\n Name: Ahmad Bin Abu\t\t\t\t\t\t\t Matric. No: MS2025123499\n\n*\t\t\t*\n** \t\t**\n***\t***\n*******\n***\t***\n**\t\t**\n*\t\t\t*\n\n\nThis is my\n\tsecond\n\t\tassignment" # Creating a multi-line string with tabs and newlines
+print(Data) # Printing the data variable
+
+full_mark = 2*10 # Assuming each question is worth 10 marks
+print( " I want 2x10 marks, which is " + str(full_mark) + " full marks \n") # Converting full_mark to string for concatenation
+# Printing the full marks with a message
+
+
