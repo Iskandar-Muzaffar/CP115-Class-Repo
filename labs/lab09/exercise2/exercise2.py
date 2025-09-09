1ed41bed145d2f062502797221ae1040b06ab410
Iskandar123123 <iskandarmuzaffar999@gmail.com>
2025-09-09 13:12:55 +0800

    update lab 9

--- labs/lab09/exercise2/exercise2.py
+++ labs/lab09/exercise2/exercise2.py
@@ -1,30 +1,30 @@
-employee_name = input("Enter employee name: ")
-base_salary = int(input("Enter base salary: "))
-overtime_hours = int(input("Enter overtime hours worked:"))
-tax_status = input("Enter tax status: ")
-
-
-epf_rate = 0.11
-sosco_rate = 0.005
-overtime_rate = 35
+employee_name = input()
+base_salary = float(input())
+overtime_hours = int(input())
+tax_status = input()
 
 # TODO your code here
-tax_rate = 0
-if tax_status == "single" and base_salary >= 5000:
-    tax_rate = 0.22
-elif tax_status == "single" and base_salary < 5000:
-    tax_rate = 0.18
-elif tax_status == "married" and base_salary >= 6000:
-    tax_rate = 0.20
-elif tax_status == "married" and base_salary < 6000:
-    tax_rate = 0.15
-elif tax_status == "head" and base_salary >= 5500:
-    tax_rate = 0.25
-elif tax_status == "head" and base_salary < 5500:
-    tax_rate = 0.19
- 
-net_salary = (base_salary + (overtime_hours * overtime_rate)) * (1 - tax_rate) * (1 - epf_rate - sosco_rate)
+
+if tax_status == "Single":
+    if base_salary >= 5000 :
+        tax_rate = 0.22
+    else:
+        tax_rate = 0.18
+elif tax_status == "Married":
+    if base_salary >= 6000:
+        tax_rate = 0.20
+    else:
+        tax_rate = 0.15
+else:
+    if base_salary >= 5500:
+        tax_rate = 0.25
+    else:
+        tax_rate = 0.19
+
+overtime_pay = 35 * overtime_hours
+gross_income = base_salary + overtime_pay
+net_salary = gross_income * (1 - tax_rate) * (1 - 0.115)
 
 print(employee_name)
-print(tax_rate)
+print(f"{tax_rate * 100:.0f}%")
 print(f"{net_salary:.2f}")
