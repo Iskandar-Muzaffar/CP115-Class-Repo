42a23df200b74180f12c3259ae0aa7361749e193
Iskandar123123 <iskandarmuzaffar999@gmail.com>
2025-09-09 12:09:54 +0800

    updae lab9

--- labs/lab09/exercise2/exercise2.py
+++ labs/lab09/exercise2/exercise2.py
@@ -1,12 +1,15 @@
 employee_name = input("Enter employee name: ")
-base_salary = float(input("Enter base salary: "))
+base_salary = int(input("Enter base salary: "))
 overtime_hours = int(input("Enter overtime hours worked:"))
 tax_status = input("Enter tax status: ")
 
-additional_deduction = 0.16
+
+epf_rate = 0.11
+sosco_rate = 0.005
+overtime_rate = 35
 
 # TODO your code here
-tax_rate = ""
+tax_rate = 0
 if tax_status == "single" and base_salary >= 5000:
     tax_rate = 0.22
 elif tax_status == "single" and base_salary < 5000:
@@ -19,8 +22,8 @@ elif tax_status == "head" and base_salary >= 5500:
     tax_rate = 0.25
 elif tax_status == "head" and base_salary < 5500:
     tax_rate = 0.19
-
-net_salary = base_salary + (overtime_hours * 35) - (base_salary * tax_rate) - ( base_salary * additional_deduction )
+ 
+net_salary = (base_salary + (overtime_hours * overtime_rate)) * (1 - tax_rate) * (1 - epf_rate - sosco_rate)
 
 print(employee_name)
 print(tax_rate)
